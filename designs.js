3cf0c0a126b3c87531bd16f76b28bc6eb3006acb
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-02-01 19:54:05 +0100

    fetch input values into variables

---
+++ designs.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    alert();
+});
+
+
+
+
+$("#sizePicker").submit(function(event){
+    event.preventDefault();
+    // Select color input
+    var color = $("#colorPicker").val();
+    
+    // Select size input
+    var height = $("#inputHeight").val();
+    var width = $("#inputWidth").val();
+});
+
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
